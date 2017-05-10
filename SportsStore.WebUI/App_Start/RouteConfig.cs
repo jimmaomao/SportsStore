3522266eb75eec67c554210cd0002b9fc98a8192
jimmaomao <493955352@qq.com>
2017-05-10 17:18:37 +0800

    列表展示和购物车初现成效1
    列表展示和购物车初现成效1

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -13,11 +13,52 @@ namespace SportsStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+//            routes.MapRoute(
+//    name: "Default",
+//    url: "{controller}/{action}/{id}",
+//    defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+//);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name:null,url:"",
+               defaults: new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
+
+            routes.MapRoute(null,"Page{page}",
+            new { controller = "Product", action = "List", category = (string)null },
+            new { page = @"\d+" }
+            );
+
+            //routes.MapRoute(
+            //name: null,
+            //url: "Page{page}",
+            //defaults: new { Controller = "Product", Action = "List" }
+            //);
+
+            routes.MapRoute(null,"{category}",
+              new { controller = "Product", action = "List", page = 1 }
+                );
+
+            routes.MapRoute(null, "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page=@"\d+"}
+                );
+    //        routes.MapRoute(null, "{controller}/{action}/{id}",
+    //new { controller = "Product", action = "List", id = UrlParameter.Optional });
+
+            routes.MapRoute(null, "{controller}/{action}");
+
+            //            routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int pageSize = 4;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;//为何加this？？
+        }
+
+        public ViewResult List(string category, int page = 1)//告诉框架为该动作渲染一个默认的视图
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products.Where(p => p.Category == null || p.Category == category)
+                .OrderBy(p => p.ProductID).Skip((page - 1) * pageSize).Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemPerPage = pageSize,
+                    TotalItems = category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            //return View(repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * pageSize).Take(pageSize));//传递products对象，给框架提供数据
+            return View(model);
+        }
+    }
+}
